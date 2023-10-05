43f1b887b114f34ded1d0159915d29a5626b05c4
Joakim Tveter <joakim@tveter.net>
2023-10-05 21:59:50 +0200

    Added persistance to Redux

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -12,9 +12,29 @@ const initialState: CartState = {
     items: 0,
 };
 
+function getPersistedCartState(): CartState {
+    try {
+        const persistedCart = localStorage.getItem('cart');
+        if (persistedCart !== null) {
+            return JSON.parse(persistedCart);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+    return initialState;
+}
+
+function persistCartState(cartState: CartState) {
+    try {
+        localStorage.setItem('cart', JSON.stringify(cartState));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 export const cartSlice = createSlice({
     name: 'cart',
-    initialState,
+    initialState: getPersistedCartState(),
     reducers: {
         addItem: (state, action: PayloadAction<CartItem>) => {
             if (state.contents.some((item) => item.id === action.payload.id)) {
@@ -28,10 +48,12 @@ export const cartSlice = createSlice({
                 state.contents.push(action.payload);
             }
             state.items += action.payload.quantity;
+            persistCartState(state);
         },
         removeItem: (state, action: PayloadAction<{ id: string; quantity: number }>) => {
             state.contents = state.contents.filter((item: any) => item.id !== action.payload.id);
             state.items = state.items - action.payload.quantity;
+            persistCartState(state);
         },
 
         addQty: (state, action: PayloadAction<{ id: string; quantity: number }>) => {
@@ -42,6 +64,7 @@ export const cartSlice = createSlice({
                 return item;
             });
             state.items += 1;
+            persistCartState(state);
         },
         subtractQty: (state, action: PayloadAction<{ id: string; quantity: number }>) => {
             if (action.payload.quantity === 0) {
@@ -54,10 +77,12 @@ export const cartSlice = createSlice({
                 return item;
             });
             state.items -= 1;
+            persistCartState(state);
         },
         resetCart: (state) => {
             state.contents = [];
             state.items = 0;
+            persistCartState(state);
         },
     },
 });
