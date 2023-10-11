b812531a97e6ab6a922135efa3d0f87d24fa25e6
Joakim Tveter <joakim@tveter.net>
2023-10-11 23:10:06 +0200

    Make cart responsive

--- src/pages/cartPage.tsx
+++ src/pages/cartPage.tsx
@@ -1,18 +1,24 @@
-import { Helmet } from 'react-helmet-async';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeItem, addQty, subtractQty } from '@/redux/cartSlice';
 
 import { formatCurrency } from '@/lib';
+import { CartItem } from '@/types/cart';
 
 import Layout from '@/components/layout';
-import CartItem from '@/components/cart-item';
 import Container from '@/components/container';
+import IconButton from '@/components/icon-button';
+import SubtractIcon from '@/icons/subtract';
+import AddIcon from '@/icons/add';
+import DeleteIcon from '@/icons/delete';
 
 import styles from './cartPage.module.css';
 
 export default function CartPage() {
     const cart = useSelector((state: any) => state.cart);
     const cartItems = cart.contents;
+    const dispatch = useDispatch();
     const cartTotal: number = cart.contents.reduce((acc: number, item: any) => acc + item.price * item.quantity, 0);
 
     return (
@@ -27,23 +33,53 @@ export default function CartPage() {
                         <p>Your cart is empty</p>
                     ) : (
                         <>
-                            <table className={styles.table} role='table'>
-                                <thead>
-                                    <tr>
-                                        <th className={styles.thItem}>Item</th>
-                                        <th className={styles.thQty}>Quantity</th>
-                                        <th className={styles.thPrice}>Price</th>
-                                        <th className={styles.thTotal}>Total</th>
-                                    </tr>
-                                </thead>
-                                <tbody role='rowgroup'>
-                                    {cartItems.map((item: any) => (
-                                        <CartItem key={item.id} {...item} />
-                                    ))}
-                                </tbody>
-                            </table>
-                            <div>
-                                <p className={styles.total}> Total: {formatCurrency(cartTotal)}</p>
+                            <ul className={styles.cartList}>
+                                {cartItems.map((item: CartItem) => (
+                                    <li key={item.id} className={styles.item}>
+                                        <Link to={`/product/${item.id}`} className={styles.details}>
+                                            <img className={styles.image} src={item.image} alt='' />
+                                            <p>{item.title}</p>
+                                        </Link>
+
+                                        <div className={styles.itemQuantity}>
+                                            <IconButton
+                                                icon={<SubtractIcon />}
+                                                label={`Remove one ${item.title} from cart`}
+                                                onClick={() =>
+                                                    dispatch(subtractQty({ id: item.id, quantity: item.quantity - 1 }))
+                                                }
+                                            />
+                                            <span style={{ paddingBottom: '5px' }}>{item.quantity}</span>
+                                            <IconButton
+                                                icon={<AddIcon />}
+                                                label={`Add one more ${item.title} to cart`}
+                                                onClick={() =>
+                                                    dispatch(addQty({ id: item.id, quantity: item.quantity + 1 }))
+                                                }
+                                            />
+                                        </div>
+                                        <p className={styles.itemPrice}>{formatCurrency(item.price)}</p>
+                                        <div className={styles.itemTotal}>
+                                            <p>{formatCurrency(item.price * item.quantity)}</p>
+                                            <IconButton
+                                                icon={<DeleteIcon color={'red'} />}
+                                                label={`Remove ${item.title} from cart`}
+                                                onClick={() =>
+                                                    dispatch(removeItem({ id: item.id, quantity: item.quantity }))
+                                                }
+                                            />
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                            <div className={styles.total}>
+                                <p> Total: {formatCurrency(cartTotal)}</p>
+                                <p>
+                                    <span>Includes 25% VAT: {formatCurrency((cartTotal / 100) * 25)}</span>
+                                </p>
+                                <p>
+                                    <span>Shipping: free </span>
+                                </p>
                             </div>
                             <div>
                                 <Link className={styles.checkout} to={'/checkout-success'}>
